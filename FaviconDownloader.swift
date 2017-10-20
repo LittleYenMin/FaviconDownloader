2890df8fc102024968aa6b90e1e2b6ab6c69fa20
邱彥銘 <fkym08@gmail.com>
2017-10-20 11:54:49 -0500

    Update FaviconDownloader.swift

--- FaviconDownloader.swift
+++ FaviconDownloader.swift
@@ -8,19 +8,23 @@
 
 import Foundation
 
+enum FavErro : Error {
+    case invalidURLString
+}
 class FaviconDownloader : NSObject {
     private static var baseURL : String = "https://www.google.com/s2/favicons?domain="
     
-    class func download(url : String , response : @escaping ((Data) -> ()) ) {
+    class func download(url : String , response : @escaping ((Data?,Error?) -> ()) ) {
         let iconURL : String = FaviconDownloader.baseURL + url
         guard let link = URL(string: iconURL) else {
+            response(nil,FavErro.invalidURLString)
             return
         }
         do {
             let imageData = try Data(contentsOf: link)
-            response(imageData)
+            response(imageData,nil)
         } catch {
-            print(error.localizedDescription)
+            response(nil,error)
         }
     }
 
